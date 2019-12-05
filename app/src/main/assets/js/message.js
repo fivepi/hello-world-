6ef022d33848fab90620af5c579398ae89cdea2d
小强 <xiaoqiang55@yahoo.com>
2019-12-05 10:53:30 +0800

    hello-world-

---
+++ app/src/main/assets/js/message.js
@@ -0,0 +1,86 @@
+
+var stompClient = null;
+var myHost = null,myUserToken = null,myCompanyId = null,myClientType = null;
+
+//重连状态&基本重连次数&当前重连次数&最大重连次数&最小重连时间
+var doReconnect = true,baseCount = 10,reconnectCount = 0,maxCount = 50,reconnectTime = 10000;
+
+//链接
+function connect(host,userToken,companyId,clientType){
+    myHost = host;
+    myUserToken = userToken;
+    myCompanyId = companyId;
+    myClientType = clientType;
+    var socket = new SockJS(host);
+    stompClient = Stomp.over(socket);
+    stompClient.debug = null;//如果需要开启stomp的调试日志,需要调试注释这行
+
+    var header = {
+    		//userToken，会员或游客的格式userToken:时间戳,刚进入网站的游客的格式  随机字符串:时间戳
+    		userToken:userToken,
+    		companyId:companyId,//厅主ID
+    		clientType:clientType//客户端类型
+    	};
+
+    	//stomp 链接
+    stompClient.connect(header, function () {
+        //js调用安卓的jsCallbackMethod方法
+        window.stub.jsCallbackMethod("连接成功");
+    	reconnectCount = 0;
+    	doReconnect = true;
+        //订阅在线人数
+        stompClient.subscribe('/user/queue/clients', function (response) {
+        	var body = JSON.parse(response.body);
+        	var data = body.data;
+        	data.virtualSize;//虚拟人数
+        	data.realSize;//实际人数
+        	//js调用安卓的jsCallbackMethod方法
+        	window.stub.jsCallbackMethod("虚拟人数" + data.virtualSize  + "实际人数" + data.realSize);
+        });
+
+        //发送消息，我上线了
+        stompClient.send('/app/online',{},null);
+
+    },function(error){
+    window.stub.jsCallbackMethod("error = " + error);
+        if (error.command == "ERROR") {
+    			//超过最大连接数，不重连
+    			window.stub.jsCallbackMethod("超过最大连接数，不重连");
+    			if (error.headers.message.indexOf('IP_MAX') != -1) doReconnect = false;
+        } else {
+    	    if(error.indexOf('Lost connection') != -1){
+    		   if(reconnectCount < maxCount){
+    			  if(reconnectCount == 0) console.log('连接已断开，重新连接');
+        		   reconnect(reconnectCount);
+    		   }
+    	    }
+    	}
+    });
+}
+
+//主动断开连接
+function disconnect(){
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    console.log('连接已断开');
+    window.stub.jsCallbackMethod("连接已断开");
+}
+
+//重连
+function reconnect(count,userToken) {
+	if (doReconnect) {
+		var timeOut = count > baseCount ?
+			reconnectTime * (count - baseCount) : reconnectTime;
+		console.log('当前第' + (reconnectCount + 1) + '次重连，等待' + (timeOut / 1000) + '秒')
+		window.stub.jsCallbackMethod('当前第' + (reconnectCount + 1) + '次重连，等待' + (timeOut / 1000) + '秒');
+		reconnectCount++;
+		setTimeout(function(){
+			connect(myHost,myUserToken,myCompanyId,myClientType);
+			window.stub.jsCallbackMethod("开始重连");
+		},timeOut)
+	}
+}
+
+
+
---
+++ app/src/main/java/com/example/administrator/stompjsandroid/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.administrator.stompjsandroid;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView = null;
+    LinearLayout linearLayout;
+    ScrollView scrollView;
+    SimpleDateFormat sDateFormat = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.linearlayout);
+        scrollView = findViewById(R.id.scrollview);
+
+        if (mWebView == null) {
+            initWebView();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String url = "javascript:connect('http://newh5.ly9900.com/message/access','33333333333:12345655555','24','3')"; //调用js方法
+                mWebView.loadUrl(url);
+            }
+        },1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cleanWebView();
+    }
+
+    /**
+     * 初始化webview
+     */
+    protected void initWebView() {
+        mWebView = new WebView(this);
+        mWebView.getSettings().setDefaultTextEncodingName("utf-8");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new JsToJava(), "stub");  //JsToJava是内部类，代码在后面。stub是接口名字。
+        mWebView.loadUrl("file:///android_asset/test.html"); //加载本地html文件
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+            }
+        });
+    }
+
+    /**
+     * js方法回调
+     */
+    private class JsToJava {
+        @JavascriptInterface
+        public void jsCallbackMethod(String result) {
+            addLog(sDateFormat.format(System.currentTimeMillis()) + "     " + result);
+        }
+    }
+
+
+    void cleanWebView() {
+        // 清WebView
+        if (mWebView != null) {
+            CookieSyncManager.createInstance(this);
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            CookieSyncManager.getInstance().sync();
+
+            mWebView.setWebChromeClient(null);
+            mWebView.setWebViewClient(null);
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mWebView.clearHistory();
+            mWebView.clearCache(true);
+            mWebView.freeMemory();
+            mWebView.removeAllViews();
+            mWebView.destroy();
+            mWebView = null;
+        }
+    }
+
+    private void addLog(final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView t = new TextView(MainActivity.this);
+                t.setText(message);
+                linearLayout.addView(t);
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+}
